20a05aaa85724ba20041114b5e4939031bb7ffaf
bnns <omethyn@gmail.com>
2018-01-07 21:17:32 -0600

    added random generation of cards

--- elm_trello/src/Main.elm
+++ elm_trello/src/Main.elm
@@ -1,81 +1,149 @@
 module Main exposing (main)
 
 import Json.Decode as Decode
+import List.Extra exposing (getAt)
 import Html exposing (..)
 import Html.Events exposing (..)
 import Html.Attributes exposing (..)
+import Random
+import Util
 
 
 type Msg
     = NoOp
-    | ShowForm ColName
-    | InputCard String
-    | AddCard ColName String
-    | HideForm
+    | LoadCards (List Card)
+    | NewCard Card
+    | InputText String
+    | ToggleAddCardForm ColId
+    | AddCardConfirm ColId
+    | ToggleEditTitleForm ColId
+    | EditTitleConfirm ColId
+    | ToggleEditCardForm CardId
+    | EditCardConfirm CardId
+    | MoveCard CardPosition CardPosition
 
 
-type ColName
-    = First
-    | Second
-    | Third
-    | Fourth
+type CardPosition
+    = ColName CardId
+
+
+type CardId
+    = CardId String
+
+
+type ColId
+    = ColId String
+
+
+type EditState
+    = EditingCard CardId
+    | AddingCard ColId
+    | EditingTitle ColId
+    | NotEditing
+
+
+type alias Identifiable a b =
+    { a | id : b }
 
 
 type alias Card =
-    String
+    { id : CardId
+    , text : String
+    , belongsTo : ColId
+    }
 
 
 type alias Column =
-    { headerColor : String
-    , cards : List Card
+    { id : ColId
+    , backgroundColor : String
+    , title : String
     }
 
 
 type alias Model =
-    { firstCol : Column
-    , secondCol : Column
-    , thirdCol : Column
-    , fourthCol : Column
-    , showForm : Maybe ColName
-    , form : String
+    { columns : List Column
+    , cards : List Card
+    , editState : EditState
+    , editText : String
     }
+
+
+initCardText : List String
+initCardText =
+    [ "pure functions", "the elm architecture", "algebraic data types", "amazingly friendly compiler messages", "elm-format", "type-safety", "first-class semantic versioning", "(model, msg) monad", "simple refactoring", "(almost) zero runtime exceptions" ]
+
+
+initColumns : List Column
+initColumns =
+    [ ( "#8e6e95", "icebox", "col1" )
+    , ( "#39a59c", "backlog", "col2" )
+    , ( "#344759", "current", "col3" )
+    , ( "#e8741e", "completed", "col4" )
+    ]
+        |> List.map (\( color, title, id ) -> Column (ColId id) color title)
+
+
+defaultCol : Column
+defaultCol =
+    Column (ColId "col0") "#fff" "default"
 
 
 initModel : Model
 initModel =
-    { firstCol = Column "#8e6e95" [ "Card 1", "Card 2" ]
-    , secondCol = Column "#39a59c" [ "Card 3", "Card 4" ]
-    , thirdCol = Column "#344759" []
-    , fourthCol = Column "#e8741e" [ "Card 5" ]
-    , showForm = Nothing
-    , form = ""
+    { columns = initColumns
+    , cards = []
+    , editState = NotEditing
+    , editText = ""
     }
-
-
-updateCardsInCol : Column -> Card -> Column
-updateCardsInCol col card =
-    { col | cards = col.cards ++ [ card ] }
-
-
-updateCol : ColName -> Card -> Model -> Model
-updateCol colName card model =
-    case colName of
-        First ->
-            { model | firstCol = updateCardsInCol model.firstCol card }
-
-        Second ->
-            { model | secondCol = updateCardsInCol model.secondCol card }
-
-        Third ->
-            { model | thirdCol = updateCardsInCol model.thirdCol card }
-
-        Fourth ->
-            { model | fourthCol = updateCardsInCol model.fourthCol card }
 
 
 clearForm : Model -> Model
 clearForm model =
-    { model | form = "" }
+    { model | editText = "" }
+
+
+toggleEdit : EditState -> EditState -> EditState
+toggleEdit current next =
+    if current == next then
+        NotEditing
+    else
+        next
+
+
+numInitialCards : Int
+numInitialCards =
+    10
+
+
+randomText : Random.Generator String
+randomText =
+    let
+        getTextAt i =
+            initCardText |> getAt (i - 1) |> Maybe.withDefault ""
+    in
+        Random.map getTextAt (Random.int 1 (List.length initCardText))
+
+
+randomCard : String -> Random.Generator Card
+randomCard text =
+    let
+        getCol i =
+            initColumns |> getAt (i - 1) |> Maybe.withDefault defaultCol
+    in
+        Random.map getCol (Random.int 1 (List.length initColumns))
+            |> Random.andThen (\col -> createCard col.id text)
+
+
+loadInitialCards : Random.Generator (List Card)
+loadInitialCards =
+    randomText
+        |> Random.andThen randomCard
+        |> Random.list 10
+
+
+createOrLoadCards : Cmd Msg
+createOrLoadCards =
+    Random.generate LoadCards loadInitialCards
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -84,17 +152,60 @@ update msg model =
         NoOp ->
             model ! []
 
-        AddCard colName val ->
-            (updateCol colName val model |> clearForm) ! []
+        ToggleAddCardForm colId ->
+            { model | editState = toggleEdit model.editState (AddingCard colId) } ! []
 
-        ShowForm colName ->
-            { model | showForm = Just colName } ! []
+        ToggleEditTitleForm colId ->
+            { model | editState = toggleEdit model.editState (EditingTitle colId) } ! []
 
-        HideForm ->
-            { model | showForm = Nothing } ! []
+        ToggleEditCardForm cardId ->
+            { model | editState = toggleEdit model.editState (EditingCard cardId) } ! []
 
-        InputCard val ->
-            { model | form = val } ! []
+        InputText val ->
+            { model | editText = val } ! []
+
+        LoadCards cards ->
+            { model | cards = cards } ! []
+
+        NewCard card ->
+            { model | cards = [ card ] ++ model.cards } ! []
+
+        AddCardConfirm colId ->
+            { model | editText = "" } ! [ Random.generate NewCard (createCard colId model.editText) ]
+
+        EditTitleConfirm colId ->
+            { model
+                | columns = updateInList (\c -> { c | title = model.editText }) colId model.columns
+                , editText = ""
+            }
+                ! []
+
+        EditCardConfirm cardId ->
+            { model
+                | cards = updateInList (\c -> { c | text = model.editText }) cardId model.cards
+                , editText = ""
+            }
+                ! []
+
+        MoveCard start end ->
+            model ! []
+
+
+createCard : ColId -> String -> Random.Generator Card
+createCard colId cardText =
+    Random.map (\id -> Card (CardId id) cardText colId) Util.randomIdString
+
+
+updateInList : (Identifiable a b -> Identifiable a b) -> b -> List (Identifiable a b) -> List (Identifiable a b)
+updateInList transform id list =
+    list
+        |> List.map
+            (\i ->
+                if i.id == id then
+                    transform i
+                else
+                    i
+            )
 
 
 subscriptions : Model -> Sub Msg
@@ -102,26 +213,34 @@ subscriptions model =
     Sub.batch []
 
 
-renderCard : Card -> Html Msg
-renderCard card =
+renderCard : EditState -> String -> Card -> Html Msg
+renderCard editState editText card =
     div [ class "card" ]
-        [ div [] [ text card ]
+        [ div [] [ text card.text ]
         ]
 
 
-renderColumn : Bool -> Msg -> String -> String -> Html Msg -> Html Msg
-renderColumn showForm addCard name color content =
-    div
-        [ class "column"
-        , style [ ( "background-color", color ) ]
-        ]
-        [ h3
-            [ class "col-title"
+renderColumn : EditState -> String -> List Card -> Column -> Html Msg
+renderColumn editState editText cards { title, id, backgroundColor } =
+    let
+        myCards =
+            cards
+                |> List.filter (\c -> c.belongsTo == id)
+                |> List.map (renderCard editState editText)
+    in
+        div
+            [ class "column"
+            , style [ ( "background-color", backgroundColor ) ]
             ]
-            [ text name ]
-        , div [] [ content ]
-        , viewIf (not showForm) (div [ onClick addCard, class "btn text-white" ] [ text "Add a card" ])
-        ]
+            [ div
+                [ class "col-title"
+                ]
+                [ span [ onClick <| ToggleEditTitleForm id, class "fas fa-pencil-alt btn" ] []
+                , div [] [ text title ]
+                ]
+            , div [] myCards
+            , viewIf (editState /= AddingCard id) (div [ onClick <| ToggleAddCardForm id, class "btn text-white" ] [ text "Add a card" ])
+            ]
 
 
 viewIf : Bool -> Html msg -> Html msg
@@ -132,56 +251,29 @@ viewIf show content =
         div [] []
 
 
-renderAddForm : Maybe ColName -> ColName -> String -> List (Html Msg) -> Html Msg
-renderAddForm showFormIn colName cardVal colHtml =
+renderAddForm : Maybe ColId -> ColId -> String -> List (Html Msg) -> Html Msg
+renderAddForm showFormIn colId formText colHtml =
     let
         form =
             div [ class "add-form" ]
-                [ textarea [ onInput <| InputCard, value cardVal ] []
-                , a [ class "btn save-btn", onClick <| AddCard colName cardVal ] [ text "Add" ]
-                , a [ class "btn cancel-btn", onClick <| HideForm ] [ text "Cancel" ]
+                [ textarea [ onInput <| InputText, value formText ] []
+                , a [ class "btn save-btn", onClick <| AddCardConfirm colId ] [ text "Add" ]
+                , a [ class "btn cancel-btn", onClick <| ToggleAddCardForm colId ] [ text "Cancel" ]
                 ]
     in
-        if showFormIn == Just colName then
+        if showFormIn == Just colId then
             div [] (colHtml ++ [ form ])
         else
             div [] colHtml
 
 
 view : Model -> Html Msg
-view model =
+view { columns, cards, editState, editText } =
     let
-        firstColView =
-            model.firstCol.cards
-                |> List.map renderCard
-                |> renderAddForm model.showForm First model.form
-                |> renderColumn (model.showForm == Just First) (ShowForm First) "icebox" model.firstCol.headerColor
-
-        secondColView =
-            model.secondCol.cards
-                |> List.map renderCard
-                |> renderAddForm model.showForm Second model.form
-                |> renderColumn (model.showForm == Just Second) (ShowForm Second) "backlog" model.secondCol.headerColor
-
-        thirdColView =
-            model.thirdCol.cards
-                |> List.map renderCard
-                |> renderAddForm model.showForm Third model.form
-                |> renderColumn (model.showForm == Just Third) (ShowForm Third) "current" model.thirdCol.headerColor
-
-        fourthColView =
-            model.fourthCol.cards
-                |> List.map renderCard
-                |> renderAddForm model.showForm Fourth model.form
-                |> renderColumn (model.showForm == Just Fourth) (ShowForm Fourth) "completed" model.fourthCol.headerColor
-
         content =
-            div [ class "row-container" ]
-                [ firstColView
-                , secondColView
-                , thirdColView
-                , fourthColView
-                ]
+            columns
+                |> List.map (renderColumn editState editText cards)
+                |> div [ class "row-container" ]
     in
         section [ id "main" ]
             [ div [ class "header" ] [ text "Trello in Elm" ]
@@ -191,7 +283,7 @@ view model =
 
 init : Decode.Value -> ( Model, Cmd Msg )
 init flags =
-    initModel ! []
+    initModel ! [ createOrLoadCards ]
 
 
 main : Program Decode.Value Model Msg
---
+++ elm_trello/src/Util.elm
@@ -0,0 +1,19 @@
+module Util exposing (randomIdString)
+
+import Random exposing (Generator)
+import Char
+
+
+randomIdString : Generator String
+randomIdString =
+    randomString 14 (char 0 127)
+
+
+char : Int -> Int -> Generator Char
+char start end =
+    Random.map Char.fromCode (Random.int start end)
+
+
+randomString : Int -> Generator Char -> Generator String
+randomString length charGenerator =
+    Random.map String.fromList (Random.list length charGenerator)
